663435f1151805be9c2a402be92092e2c69749d2
Piri-kara <hpt_q@yahoo.co.jp>
2020-05-11 19:54:19 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+function fib(n){
+  if(n === 0){
+    return 0;
+  } else if (n === 1){
+    return 1;
+  }
+
+  // 関数の中で関数自信を呼び出すことを 再帰 という。
+  return fib(n - 1) + fib(n - 2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++){
+  console.log(fib(i))
+}
